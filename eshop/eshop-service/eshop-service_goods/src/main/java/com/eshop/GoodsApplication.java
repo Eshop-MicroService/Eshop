a2dbe04da14274210e72428a8b41c1ad0aa20e34
Owarimasu <401205991@qq.com>
2020-11-14 15:43:35 +0800

    商品微服务构建
    将eshop_goods数据库表生成的pojo导入项目，注册商品微服务

---
+++ eshop/eshop-service/eshop-service_goods/src/main/java/com/eshop/GoodsApplication.java
@@ -0,0 +1,14 @@
+package com.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class,args);
+    }
+}
